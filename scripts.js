f89b45c3f179482612948560cf1abfdf0ae585cc
jolega <alonso_juancho@hotmail.com>
2022-02-14 09:59:13 -0500

    commit inicial

---
+++ scripts.js
@@ -0,0 +1,15 @@
+//Importacion de Modulos, 
+
+//Delcaracion de Variable, 
+var arreglo = [10,11,20,5,"facultad"]
+
+
+//Declaracion de Funciones,
+
+function cambiarValor(paremetro1 , par){
+    return "cambio"
+}
+
+//Ejecucion del Codigo
+arreglo[4]=cambiarValor();
+console.log(arreglo)
